899eca20a29d21201b515b3bdcb5cd0215086fc1
Rafael Contreras <rafcontreras@users.noreply.github.com>
2018-08-06 10:58:29 +1200

    Update RNUXCam.h

--- ios/RNUXCam.h
+++ ios/RNUXCam.h
@@ -6,12 +6,7 @@
 //  Copyright © 2016 Mark Miyashita. All rights reserved.
 //
 
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#else // back compatibility for RN version < 0.40
-#import "RCTBridgeModule.h"
-#endif
-
 
 @interface RNUXCam : NSObject <RCTBridgeModule>
 @end
